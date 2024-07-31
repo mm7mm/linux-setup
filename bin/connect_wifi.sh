0f487986a0515581e9c46499caaff413921d7c6a
mm7mm <mohamedapdalrazeq@gmail.com>
2024-08-01 00:34:57 +0300

    bin

--- bin/connect_wifi.sh
+++ bin/connect_wifi.sh
@@ -1,50 +1,36 @@
 #!/bin/bash
 
-# Check if the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root."
-  exit 1
-fi
+# تعيين اسم واجهة الشبكة
+INTERFACE="wlp2s0"
+#!/bin/bash
 
-# Terminate any existing wpa_supplicant process
-doas pkill wpa_supplicant
-echo "Terminated any existing wpa_supplicant process."
+# تعيين اسم واجهة الشبكة
+INTERFACE="wlp2s0"
+CONFIG_FILE="/etc/wpa_supplicant/wpa_supplicant.conf"
 
-# Unblock Wi-Fi
+# إلغاء قفل واجهة Wi-Fi إذا كانت محجوزة
+echo "Unblocking Wi-Fi..."
 doas rfkill unblock wifi
-echo "Unblocked Wi-Fi."
 
-# Remove any existing wpa_supplicant control interface file
-doas rm -f /run/wpa_supplicant/wlp2s0
-echo "Removed any existing wpa_supplicant control interface file."
+# إيقاف أي عملية wpa_supplicant قيد التشغيل
+echo "Stopping any existing wpa_supplicant processes..."
+doas killall wpa_supplicant
 
-# Enable Wi-Fi interface
-doas ip link set wlp2s0 up
-echo "Enabled Wi-Fi interface."
-
-# Wait a few seconds to ensure the interface is up
-sleep 5
-
-# Configure the Wi-Fi connection using wpa_supplicant
-doas wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-if [ $? -eq 0 ]; then
-  echo "Configured Wi-Fi connection using wpa_supplicant."
-else
-  echo "Failed to configure Wi-Fi connection using wpa_supplicant."
-  exit 1
+# حذف ملفات واجهة التحكم المتبقية إذا كانت موجودة
+CONTROL_FILE="/run/wpa_supplicant/$INTERFACE"
+if [ -e "$CONTROL_FILE" ]; then
+    echo "Deleting existing control interface file..."
+    doas rm "$CONTROL_FILE"
 fi
 
-# Wait a few seconds for wpa_supplicant to establish connection
-sleep 5
+# تشغيل wpa_supplicant مع التكوين المحدد
+echo "Starting wpa_supplicant..."
+doas wpa_supplicant -B -i "$INTERFACE" -c "$CONFIG_FILE"
 
-# Obtain an IP address using dhcpcd
-doas dhcpcd wlp2s0
+# تحقق من حالة التشغيل
 if [ $? -eq 0 ]; then
-  echo "Obtained an IP address using dhcpcd."
+    echo "wpa_supplicant started successfully."
 else
-  echo "Failed to obtain an IP address using dhcpcd."
-  exit 1
+    echo "Failed to start wpa_supplicant."
 fi
-
-echo "Successfully connected to Wi-Fi."
 
